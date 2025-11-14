0058d3d0b1421d2deed9a2d038191e6e533ba344
Harihdp00 <harihdp00@gmail.com>
2025-11-14 22:06:23 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,15 @@
 output "private_key_path" {
-  description = "Local private key path created by Terraform"
-  value       = local_file.private_key_pem.filename
+  value = local_file.private_key.filename
 }
 
 output "control_public_ip" {
-  description = "Control node public IP"
-  value       = aws_instance.control.public_ip
+  value = aws_instance.control.public_ip
 }
 
 output "jenkins_master_public_ip" {
-  description = "Jenkins master public IP"
-  value       = aws_instance.jenkins_master.public_ip
+  value = aws_instance.master.public_ip
 }
 
 output "jenkins_worker_public_ip" {
-  description = "Jenkins worker public IP"
-  value       = aws_instance.jenkins_worker.public_ip
+  value = aws_instance.worker.public_ip
 }
